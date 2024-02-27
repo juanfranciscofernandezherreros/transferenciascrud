32de06e4a969aef9a2c3620ec3a5e25f71a2aaf2
Your Name <juanfranciscofernandezherreros>
2024-02-27 22:46:55 +0100

    Paso 1: Configuración del Proyecto

---
+++ src/main/java/com/ejemplo/transferenciascrud/TransferenciascrudApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.transferenciascrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciascrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciascrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/transferenciascrud/TransferenciascrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.transferenciascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciascrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
